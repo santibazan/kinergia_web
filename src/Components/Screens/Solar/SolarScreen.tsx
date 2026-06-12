b0c510934b37a61cf78a1b1586b6b23cefbdb828
santi bazan <santiago.n.bazan@outlook.com>
2026-06-12 13:02:39 -0300

    solucionando error de deploy

--- src/Components/Screens/Solar/SolarScreen.tsx
+++ src/Components/Screens/Solar/SolarScreen.tsx
@@ -1,15 +1,15 @@
-import FooterUI from "../../UI/Footer/FooterUI";
-import NavbarUI from "../../UI/Header/NavbarUI";
-import SolarUI from "../../UI/Solar/SolarUI";
+// import FooterUI from "../../UI/Footer/FooterUI";
+// import NavbarUI from "../../UI/Header/NavbarUI";
+// import SolarUI from "../../UI/Solar/SolarUI";
 
 
 
-export const SolarScreen = () => {
-  return (
-    <>
-      <NavbarUI />
-      <SolarUI/>
-      <FooterUI />
-    </>
-  );
-};
+// export const SolarScreen = () => {
+//   return (
+//     <>
+//       <NavbarUI />
+//       <SolarUI/>
+//       <FooterUI />
+//     </>
+//   );
+// };
